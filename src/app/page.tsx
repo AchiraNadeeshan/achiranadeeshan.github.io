d0e4cc9089afafe207713d4ae8ebeeb6901d4d5e
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-12-06 18:47:24 +0530

    chore: remove unnecessary template data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import { api, HydrateClient } from "~/trpc/server";
 
-export default async function Home() {
+export default async function home() {
 
   return (
     <HydrateClient>
