c9a6941cd1ff8f7f0249197694c83ae0eb34c57e
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-12-06 18:43:45 +0530

    refactor: move pages content to app directory

--- src/app/pages/experience/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function experience() {
-  return (
-    <main className="p-8">
-        <h1 className="text-3xl font-bold">My Experience</h1>
-        <p>These are the highlights of my professional journey and skills I've acquired over the years.</p>
-    </main>
-  );
-}
--- src/app/pages/projects/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function projects() {
-  return (
-    <main className="p-8">
-        <h1 className="text-3xl font-bold">My Projects</h1>
-        <p>Here are some of the projects I've worked on.</p>
-    </main>
-  );
-}
