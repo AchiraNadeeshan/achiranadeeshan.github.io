1bbfaf2fe721a3fa8deab9f3284d22049e8ca3ec
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-12-06 17:43:02 +0530

    create initial folder structure

---
+++ src/app/pages/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function blog() {
+  return (
+    <main className="p-8">
+        <h1 className="text-3xl font-bold">My Blog</h1>
+        <p>Welcome to my blog. Here I share stuff I learn throughout my development journey</p>
+    </main>
+  );
+}
