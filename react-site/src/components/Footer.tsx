a6f8de3fdc3edf4385c1a73b90abc5215614dc03
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2026-02-08 08:08:57 +0530

    chore: update my webring slag

--- react-site/src/components/Footer.tsx
+++ react-site/src/components/Footer.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export function Footer() {
   return (
     <div className="mx-auto py-10">
-      <a href="https://ctp-webr.ing/YOUR_SLUG/previous">&larr;</a>
+      <a href="https://ctp-webr.ing/AchiraNadeeshan/previous">&larr;</a>
       <a href="https://ctp-webr.ing/"> Catppuccin webring </a>
-      <a href="https://ctp-webr.ing/YOUR_SLUG/next">&rarr;</a>
+      <a href="https://ctp-webr.ing/AchiraNadeeshan/next">&rarr;</a>
     </div>
   );
 }
